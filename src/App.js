f444f5bfe2dee430a7ceb84bcefa0ff90c364043
Furkan <furkanktnci@gmail.com>
2023-10-19 13:36:37 +0300

    I put the testing stages and first test writing into code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <div>Testing</div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />); // Arrange aşaması denilebilir.
+//   const linkElement = screen.getByText(/learn react/i); // Act aşaması denilebilir. (Bir butona tıklamak örnek verilebilir.)
+//   expect(linkElement).toBeInTheDocument(); // assertion aşaması denilebilir.
+// });
+
+test(
+  "test'in ilk parametresidir ve string bir değer alır. Bu kısımda test gerçekleştiğinde neyi amaçlıyorsak onu yazarız. (Testin açıklaması)",
+  () => {"testin ikinci parametresi ise bir function. Bu function içerisinde test'in çalışmasını sağlayan bir function yazarız."},
+)
+
+
+it("component başarılı bir sekilde render edilmeli", () =>{
+  render(<App />) // App componentini render et.
+  expect(screen.getByText("Testing")).toBeInTheDocument(); 
+  // getByText ile div içerisindeki yazıyı alıyorum.
+  // ekranda div içerisinde "Testing" yazmasını bekliyorum. Eğer yazıyorsa sorun yok yazmıyorsa test "fail" oluyor.
+}) 
